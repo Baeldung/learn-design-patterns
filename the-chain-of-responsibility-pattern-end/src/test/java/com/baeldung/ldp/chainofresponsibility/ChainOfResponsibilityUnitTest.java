9ff03fe90ec83a595ffe38e0a40634c9e6b26915
Baeldung CI User <baeldungci@baeldung.com>
2026-07-26 06:53:00 +0000

    LDP M3-L5: join single-line setNext chains in unit test (author's local style edit)

--- the-chain-of-responsibility-pattern-end/src/test/java/com/baeldung/ldp/chainofresponsibility/ChainOfResponsibilityUnitTest.java
+++ the-chain-of-responsibility-pattern-end/src/test/java/com/baeldung/ldp/chainofresponsibility/ChainOfResponsibilityUnitTest.java
@@ -1,7 +1,6 @@
 package com.baeldung.ldp.chainofresponsibility;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +9,9 @@ class ChainOfResponsibilityUnitTest {
     @Test
     void givenSmallRequest_whenHandled_thenApprovedByTeamLead() {
         AbstractApprovalHandler chain = new TeamLeadHandler();
-        chain.setNext(new ManagerHandler())
-          .setNext(new DirectorHandler());
-        ApprovalRequest request = new ApprovalRequest(500, "New keyboard");
+        chain.setNext(new ManagerHandler()).setNext(new DirectorHandler());
 
+        ApprovalRequest request = new ApprovalRequest(500, "New keyboard");
         chain.handle(request);
 
         assertEquals("TeamLead", request.getApprovedBy());
@@ -22,10 +20,9 @@ class ChainOfResponsibilityUnitTest {
     @Test
     void givenMidSizeRequest_whenHandled_thenApprovedByManager() {
         AbstractApprovalHandler chain = new TeamLeadHandler();
-        chain.setNext(new ManagerHandler())
-          .setNext(new DirectorHandler());
-        ApprovalRequest request = new ApprovalRequest(3_000, "Team offsite");
+        chain.setNext(new ManagerHandler()).setNext(new DirectorHandler());
 
+        ApprovalRequest request = new ApprovalRequest(3_000, "Team offsite");
         chain.handle(request);
 
         assertEquals("Manager", request.getApprovedBy());
@@ -34,35 +31,21 @@ class ChainOfResponsibilityUnitTest {
     @Test
     void givenLargeRequest_whenHandled_thenApprovedByDirector() {
         AbstractApprovalHandler chain = new TeamLeadHandler();
-        chain.setNext(new ManagerHandler())
-          .setNext(new DirectorHandler());
-        ApprovalRequest request = new ApprovalRequest(15_000, "Conference sponsorship");
+        chain.setNext(new ManagerHandler()).setNext(new DirectorHandler());
 
+        ApprovalRequest request = new ApprovalRequest(15_000, "Conference sponsorship");
         chain.handle(request);
 
         assertEquals("Director", request.getApprovedBy());
     }
 
     @Test
-    void givenOverLimitRequest_whenHandled_thenApprovedByIsNull() {
-        AbstractApprovalHandler chain = new TeamLeadHandler();
-        chain.setNext(new ManagerHandler())
-          .setNext(new DirectorHandler());
-        ApprovalRequest request = new ApprovalRequest(50_000, "Acquisition");
-
-        chain.handle(request);
-
-        assertNull(request.getApprovedBy());
-    }
-
-    @Test
     void givenOverLimitRequest_whenDefaultHandlerAppended_thenApprovedByManualReview() {
         AbstractApprovalHandler chain = new TeamLeadHandler();
-        chain.setNext(new ManagerHandler())
-          .setNext(new DirectorHandler())
-          .setNext(new DefaultApprovalHandler());
-        ApprovalRequest request = new ApprovalRequest(50_000, "Acquisition");
+        chain.setNext(new ManagerHandler()).setNext(new DirectorHandler())
+            .setNext(new DefaultApprovalHandler());
 
+        ApprovalRequest request = new ApprovalRequest(50_000, "Acquisition");
         chain.handle(request);
 
         assertEquals("ManualReview", request.getApprovedBy());
